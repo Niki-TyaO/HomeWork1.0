1240daa4afbc56f1380464ecb1edd247f0ea1cb5
Niki-TyaO <116357268+Niki-TyaO@users.noreply.github.com>
2024-10-14 21:15:30 +0800

    Add files via upload

---
+++ module_3_2.py
@@ -0,0 +1,30 @@
+recipient = ''
+message = ''
+def send_mail(recipient, messege, sender ='university.help@gmail.com'):
+    if '@' not in recipient:
+        checker = False
+    elif '.ru' or '.com' or '.net' in recipient:
+        checker = True
+    else:
+        checker = True
+
+    if checker == False:            #чего это он ругается?
+
+        print('Невозможно отправить письмо с адреса ', sender, 'на адрес ', recipient)
+
+    elif sender == recipient:
+
+        print('Нельзя отправить письмо самому себе!')
+
+    elif sender == 'university.help@gmail.com':
+
+        print('Письмо успешно отправлено с адреса ', sender,  ' на адрес ', recipient)
+
+    elif sender != 'university.help@gmail.com':
+
+        print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса ', sender,  ' на адрес ', recipient)
+
+send_mail('universitygmail.com', 'Здравствуйте!')
+send_mail('university.help@gmail.com', 'Здравствуйте!')
+send_mail('university@gmail.com', 'Здравствуйте!')
+send_mail('university@gmail.com', 'Здравствуйте!','none@mail.ru')
